1abc4c8b86019a49a0545f932c0da65aefb52c5d
Adarsh S <adarshs2500@gmail.com>
2025-08-12 00:03:38 +0530

    image size and heading

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -57,14 +57,14 @@ function ProjectsSection() {
           <ImageCarousel />
         </div>
       </div>
-        <div className="about-section" >
+      <div className="about-section" >
         {/* First Row: About Us */}
         <div className="about-content-wrapper">
           <div className="about-text">
             <span className="about-us-label">About Us</span>
             <div style={{ height: '3px', width: '50px', background: '#f7842b', margin: '8px 0 16px 0', borderRadius: '2px' }} />
-            <h2 className="about-title">Who are we?</h2>
-            <p className="about-desc">
+            <h2 className="about-title1">Who are we?</h2>
+            <p className="about-desc1">
               AMES is a dynamic platform by and for Mechanical Engineering students at NITK. We empower each other with hands-on learning, industry insights, and lifelong connections.
             </p>
             <button className="about-btn">Know More</button>
@@ -73,7 +73,6 @@ function ProjectsSection() {
             <img src={aboutImage} alt="Graduation" />
           </div>
         </div>
-      
         {/* Second Row: Objectives */}
         <div className="about-content-wrapper" style={{ marginTop: "2.5rem" }}>
           <div className="about-image2">
@@ -82,14 +81,15 @@ function ProjectsSection() {
           <div className="about-text2">
             <span className="about-us-label">Objectives</span>
             <div style={{ height: '3px', width: '50px', background: '#f7842b', margin: '8px 0 16px 0', borderRadius: '2px' }} />
-            <h2 className="about-title" >Objectives That Drives Us</h2>
-            <ul className="about-desc" style={{ marginBottom: "1.5rem" }}>
+            <h2 className="about-title1" >Objectives That Drives Us</h2>
+            <ul className="about-desc2" style={{ marginBottom: "1.5rem" }}>
               <li>Cultivate a culture of engineering excellence through shared knowledge and collaborative initiatives.</li>
               <li>Bridge the academia-industry gap via projects & mentorship.</li>
               <li>Promote interdisciplinary research & social innovation</li>
             </ul>
             <button className="about-btn">Explore</button>
           </div>
+        </div>
       </div>
       {/* <div className="featured-projects-nitk" ref={eventRef}> */}
       {/* <div className="fp-content-left">
@@ -154,7 +154,7 @@ function ProjectsSection() {
             )}
           </div> */}
       {/* </div> */}
-    </div>
+
     </section >
   );
 }
